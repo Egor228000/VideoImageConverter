720ee27cba405ba2be0dcd445437ade6e860915b
egork <egorkrasnov123456789@gmail.com>
2025-07-24 13:40:19 +0700

    fix

--- composeApp/src/desktopMain/kotlin/org/example/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/main.kt
@@ -1,8 +1,13 @@
 package org.example.project
 
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.mayakapps.compose.windowstyler.WindowBackdrop
 import com.mayakapps.compose.windowstyler.WindowCornerPreference
 import com.mayakapps.compose.windowstyler.WindowFrameStyle
@@ -11,9 +16,15 @@ import io.github.vinceglb.filekit.dialogs.FileKitDialogSettings
 import java.awt.Dimension
 
 fun main() = application {
+    val state = rememberWindowState(
+        position = WindowPosition(Alignment.Center),
+        size = DpSize(700.dp, 900.dp)
+    )
     Window(
         onCloseRequest = ::exitApplication,
         title = "VideoImageConverter",
+        state = state,
+
     ) {
         WindowStyle(
             isDarkTheme = true,
