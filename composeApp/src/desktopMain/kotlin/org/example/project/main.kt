31eeb478c540722fb19240ba2f6a38e940f25437
egork <egorkrasnov123456789@gmail.com>
2025-07-24 13:21:29 +0700

    точка входы программы и кастомное окно(оформление)

--- composeApp/src/desktopMain/kotlin/org/example/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/main.kt
@@ -1,13 +1,28 @@
 package org.example.project
 
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.mayakapps.compose.windowstyler.WindowBackdrop
+import com.mayakapps.compose.windowstyler.WindowCornerPreference
+import com.mayakapps.compose.windowstyler.WindowFrameStyle
+import com.mayakapps.compose.windowstyler.WindowStyle
+import io.github.vinceglb.filekit.dialogs.FileKitDialogSettings
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "VideoImageConverter",
     ) {
-        App()
+        WindowStyle(
+            isDarkTheme = true,
+            backdropType = WindowBackdrop.Mica,
+            frameStyle = WindowFrameStyle(cornerPreference = WindowCornerPreference.ROUNDED),
+        )
+        val dialogSettings = FileKitDialogSettings(this.window)
+        window.minimumSize = Dimension(700, 900)
+        val appViewModel = rememberSaveable { AppViewModel() }
+        App(appViewModel, dialogSettings)
     }
 }
